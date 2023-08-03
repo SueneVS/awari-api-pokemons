932276ce6125c1f7de5ed71ac5f0e4c4cf909a0c
Suene <suene.vs@gmail.com>
2023-08-03 17:58:07 -0300

    feat:setup end-to-end test

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  viewportWidth: 1366,
+  viewportHeight: 768,
+  defaultCommandTimeout: 10000,
+
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    testIsolation: false,
+  },
+
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+  },
+});
