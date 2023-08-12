a51797a97a78c97748254c2dd066bade72e1a843
Suene <suene.vs@gmail.com>
2023-08-12 16:45:26 -0300

    feat: add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,24 +2,32 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-     es2020: true,
-     jest: true
-   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    'plugin:cypress/recommended'
+    es2020: true,
+    jest: true
+  },
+  extends: ['eslint:recommended', 
+    'plugin:react/recommended', 
+    'plugin:react/jsx-runtime', 
+    'plugin:react-hooks/recommended', 
+    'plugin:cypress/recommended', 
+    'plugin:storybook/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: '18.2'
+    }
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components':'warn',
+    'react-refresh/only-export-components': 'warn',
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
-		'react/prop-types': 'off'
-  },
-}
+    'react/prop-types': 'off',
+    "import/no-anonymous-default-export": "off"
+  }
+};
